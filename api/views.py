09342ed17d64c3d83b3df60ff055b6fb2a8338cc
Luca Querci <lucabit73@gmail.com>
2018-12-04 14:32:31 +0100

    simply return JSON

--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 def index(request):
-    return HttpResponse("Hello my friend!!")
+    resp = {'data': [{'num': 1}, {'num': 2}]}
+    return JsonResponse(resp)
